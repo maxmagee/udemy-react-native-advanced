472735a210e23602ffc634951f64e54c4c217d95
Max Magee <maxmagee@users.noreply.github.com>
2018-07-06 21:51:16 -0500

    Created a list of cards that pull in the static data.

--- swipe/App.js
+++ swipe/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { Button, Card } from 'react-native-elements';
 
-import Ball from './src/Ball';
+import Deck from './src/Deck';
+
+import CARD_DATA from './src/data/cards';
 
 export default class App extends React.Component {
+  renderCard = item => {
+    return (
+      <Card key={item.id} title={item.text} image={{ uri: item.uri }}>
+        <Text style={{ marginBottom: 10 }}>I can customize the card further</Text>
+        <Button icon={{ name: 'code' }} backgroundColor="#03A9F4" title="View Now!" />
+      </Card>
+    );
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Ball />
-      </View>
+      <SafeAreaView style={styles.container}>
+        <Deck data={CARD_DATA} renderCard={this.renderCard} />
+      </SafeAreaView>
     );
   }
 }
---
+++ swipe/src/Deck.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Animated, View } from 'react-native';
+
+class Deck extends Component {
+  renderCards = () => {
+    return this.props.data.map(item => {
+      return this.props.renderCard(item);
+    });
+  };
+
+  render() {
+    return <View>{this.renderCards()}</View>;
+  }
+}
+
+export default Deck;
